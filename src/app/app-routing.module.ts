6353e83cf9e2b9646f4497e49a6adcb9a1f15607
Jamerson Carlos <64469941+JamersonCarlos@users.noreply.github.com>
2022-08-30 14:15:59 -0300

    Ambiente de Cadastro de equipamentos Criado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
+import { importType } from '@angular/compiler/src/output/output_ast';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { CadastroEquipamentoComponent } from './components/cadastro-equipamento/cadastro-equipamento.component';
+import { CadastroLaboratorioComponent } from './components/cadastro-laboratorio/cadastro-laboratorio.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { 
+    path: 'cadastroEquipamento',
+    component: CadastroEquipamentoComponent 
+  },
+  { 
+    path: 'cadastroLaboratorio',
+    component: CadastroLaboratorioComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
