a166582effb85c64c93c52ad282d6ed4b891cd68
hellyaxs <eliasvitor.dev@gmail.com>
2022-08-31 19:01:18 -0300

    definido variavel de ambiente

--- src/app/services/equipamento.service.ts
+++ src/app/services/equipamento.service.ts
@@ -1,36 +1,37 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 import { Equipamento } from '../interfaces/equipamento';
-
-const url ="http://localhost:8082/equipamento/";
 
 @Injectable({
   providedIn: 'root'
 })
 
 export class EquipamentoService {
+
+  private readonly API = `${environment.API}equipamento/`
  
   constructor(private http: HttpClient) { }
 
   public postEquipamento(boydEquipamento:Equipamento):Observable<Equipamento> {
-      return this.http.post<Equipamento>(url,boydEquipamento);
+      return this.http.post<Equipamento>(this.API,boydEquipamento);
   }
 
   public putEquipamento(id:number,boydEquipamento:Equipamento) {
-    this.http.put<Equipamento>(url+id,boydEquipamento)
+    this.http.put<Equipamento>(this.API+id,boydEquipamento)
   }
 
   public delete(id:number){
-    this.http.delete<Equipamento>(url+id)
+    this.http.delete<Equipamento>(this.API+id)
   }
 
   public getEquipamentobyId(id:number): Observable<Equipamento> {
-    return this.http.get<Equipamento>(url+id);
+    return this.http.get<Equipamento>(this.API+id);
   }
 
   public getEquipamento():Observable<Equipamento> {
-    return this.http.get<Equipamento>(url);
+    return this.http.get<Equipamento>(this.API);
   }
 
 
