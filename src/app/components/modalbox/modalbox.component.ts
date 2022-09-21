ec629ee6f9cb47f031d725a7a00c1f63a91dd6c3
hellyaxs <eliasvitor.dev@gmail.com>
2022-09-21 10:51:56 -0300

    modalbox

---
+++ src/app/components/modalbox/modalbox.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-modalbox',
+  templateUrl: './modalbox.component.html',
+  styleUrls: ['./modalbox.component.css']
+})
+export class ModalboxComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
